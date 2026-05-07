401f18781735ad448ea79cab4437e9e1f59db567
marvimssantos <marvim.ssantos@gmail.com>
2026-05-06 22:57:12 -0300

    Update servico_emprestimo.py

--- services/servico_emprestimo.py
+++ services/servico_emprestimo.py
@@ -1,1 +1,10 @@
+class ServicoEmprestimo:
 
+    def registrar(self, equip_id, nome, email, dias):
+        ...
+
+    def registrar_devolucao(self, id):
+        ...
+
+    def listar_atrasados(self):
+        ...

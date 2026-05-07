adb8bc1a7e450790f62e261ee91688e0ae383a6d
marvimssantos <marvim.ssantos@gmail.com>
2026-05-06 22:58:17 -0300

    Update repositorio_emprestimo.py

--- repositories/repositorio_emprestimo.py
+++ repositories/repositorio_emprestimo.py
@@ -1,1 +1,10 @@
+class RepositorioEmprestimo:
 
+    def buscar_equipamento(self, equip_id):
+        ...
+
+    def salvar_emprestimo(self, emprestimo):
+        ...
+
+    def buscar_emprestimos(self):
+        ...

809c651dad3da41ceb916b6175a34b06efcc524a
marvimssantos <marvim.ssantos@gmail.com>
2026-05-06 22:56:13 -0300

    Update emprestimo.py

--- models/emprestimo.py
+++ models/emprestimo.py
@@ -1,1 +1,11 @@
+from dataclasses import dataclass
+from datetime import date
 
+@dataclass
+class Emprestimo:
+    id: int
+    equipamento_id: int
+    nome_usuario: str
+    email: str
+    data_devolucao: date
+    devolvido: bool

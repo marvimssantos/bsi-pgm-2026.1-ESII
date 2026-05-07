8731073442b4af4b13c5c05af03a370ac4ef552d
marvimssantos <marvim.ssantos@gmail.com>
2026-05-06 22:57:45 -0300

    Update notificador.py

--- services/notificador.py
+++ services/notificador.py
@@ -1,1 +1,10 @@
+class Notificador:
 
+    def notificar_emprestimo(self, email, data_devolucao):
+        ...
+
+    def notificar_atraso(self, email):
+        ...
+
+    def enviar_email(self, mensagem):
+        ...

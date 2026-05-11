2e2571bca9a29436e79425ed4b56eb5255c0c3c2
marvimssantos <marvim.ssantos@gmail.com>
2026-05-11 15:56:58 -0300

    Update main.py
    atividade 4b

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# Menu será implementado futuramente
+from services.servico_emprestimo import ServicoEmprestimo
+
+servico = ServicoEmprestimo()
+
+servico.registrar(
+    1,
+    "Marcos",
+    "viniciusmv25@gmail.com",
+    7
+)
+
+print(servico.listar_atrasados())

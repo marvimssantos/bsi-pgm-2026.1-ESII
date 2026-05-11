1e5069fbf3a3459a40304a786627f1086dbd8ac1
marvimssantos <marvim.ssantos@gmail.com>
2026-05-11 15:53:01 -0300

    Update repositorio_emprestimo.py
    atividade 4b

--- repositories/repositorio_emprestimo.py
+++ repositories/repositorio_emprestimo.py
@@ -1,10 +1,36 @@
+from models.equipamento import Equipamento
+from models.emprestimo import Emprestimo
+
 class RepositorioEmprestimo:
 
+    def __init__(self):
+        self.equipamentos = [
+            Equipamento(1, "Notebook", True),
+            Equipamento(2, "Projetor", True)
+        ]
+
+        self.emprestimos = []
+
     def buscar_equipamento(self, equip_id):
-        ...
+        for equipamento in self.equipamentos:
+            if equipamento.id == equip_id:
+                return equipamento
+        return None
 
     def salvar_emprestimo(self, emprestimo):
-        ...
+        self.emprestimos.append(emprestimo)
 
     def buscar_emprestimos(self):
-        ...
+        return self.emprestimos
+
+    def marcar_indisponivel(self, equip_id):
+        equipamento = self.buscar_equipamento(equip_id)
+
+        if equipamento:
+            equipamento.disponivel = False
+
+    def marcar_disponivel(self, equip_id):
+        equipamento = self.buscar_equipamento(equip_id)
+
+        if equipamento:
+            equipamento.disponivel = True

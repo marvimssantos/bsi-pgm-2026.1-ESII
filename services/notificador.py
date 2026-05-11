bec765835bda797519acb583eb98ca7c643069c2
marvimssantos <marvim.ssantos@gmail.com>
2026-05-11 15:53:50 -0300

    Update notificador.py
    atividade 4b

--- services/notificador.py
+++ services/notificador.py
@@ -1,10 +1,12 @@
 class Notificador:
 
     def notificar_emprestimo(self, email, data_devolucao):
-        ...
+        mensagem = f"Empréstimo registrado. Devolução: {data_devolucao}"
+        self.enviar_email(mensagem)
 
     def notificar_atraso(self, email):
-        ...
+        mensagem = "Seu empréstimo está atrasado."
+        self.enviar_email(mensagem)
 
     def enviar_email(self, mensagem):
-        ...
+        print(f"[EMAIL] {mensagem}")

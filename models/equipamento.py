88ed08c8743875c1089b1d2a1fadcc29114e6c22
marvimssantos <marvim.ssantos@gmail.com>
2026-05-17 16:36:39 -0300

    Update equipamento.py
    atividade 05

--- models/equipamento.py
+++ models/equipamento.py
@@ -1,8 +1,29 @@
-# Equipamento: representar um equipamento do sistema.
+# Equipamento: representar equipamentos do sistema.
+
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
+
 @dataclass
-class Equipamento:
+class Equipamento(ABC):
     id: int
     nome: str
     disponivel: bool
+
+    @abstractmethod
+    def calcular_multa(self, dias_atraso):
+        pass
+
+
+@dataclass
+class Notebook(Equipamento):
+
+    def calcular_multa(self, dias_atraso):
+        return max(0, dias_atraso * 10)
+
+
+@dataclass
+class Projetor(Equipamento):
+
+    def calcular_multa(self, dias_atraso):
+        return max(0, dias_atraso * 5)

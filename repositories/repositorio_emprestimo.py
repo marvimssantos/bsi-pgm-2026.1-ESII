0f3e4be1e413688b3b0389f5c15324972ec41a75
marvimssantos <marvim.ssantos@gmail.com>
2026-05-17 16:39:16 -0300

    Update repositorio_emprestimo.py
    atividade 05

--- repositories/repositorio_emprestimo.py
+++ repositories/repositorio_emprestimo.py
@@ -1,21 +1,27 @@
 # RepositorioEmprestimo: persistir dados de empréstimos.
-from models.equipamento import Equipamento
+
+from models.equipamento import Notebook, Projetor
 from models.emprestimo import Emprestimo
+
 
 class RepositorioEmprestimo:
 
     def __init__(self):
+
         self.equipamentos = [
-            Equipamento(1, "Notebook", True),
-            Equipamento(2, "Projetor", True)
+            Notebook(1, "Notebook", True),
+            Projetor(2, "Projetor", True)
         ]
 
         self.emprestimos = []
 
     def buscar_equipamento(self, equip_id):
+
         for equipamento in self.equipamentos:
+
             if equipamento.id == equip_id:
                 return equipamento
+
         return None
 
     def salvar_emprestimo(self, emprestimo):
@@ -25,12 +31,14 @@ class RepositorioEmprestimo:
         return self.emprestimos
 
     def marcar_indisponivel(self, equip_id):
+
         equipamento = self.buscar_equipamento(equip_id)
 
         if equipamento:
             equipamento.disponivel = False
 
     def marcar_disponivel(self, equip_id):
+
         equipamento = self.buscar_equipamento(equip_id)
 
         if equipamento:
